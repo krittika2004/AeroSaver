35793ca02413d98b0426275501582124e754ce1c
Kanavpreet-Singh <kanavpreetsingh2005@gmail.com>
2025-02-14 11:33:08 +0530

    added UI for 404 page not found

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,13 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use("/api/predictions", predictionRoutes());
+
 app.get("/", (req, res)=>{
     res.send("This is home page");
 })
+
+
+
 app.listen(PORT, ()=>{
     console.log(`Server is listening on port ${PORT}`);
 })
